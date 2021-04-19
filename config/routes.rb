65f11162a5fe98258b572f96d52a69eb7529d360
Facundo Espinosa <facundo.espinosa@fing.edu.uy>
2021-04-19 17:40:20 -0300

    Setup rubocop and linter (#3)
    * Setup rubocop
    
    * Setup github action CI linter

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,31 @@
+# frozen_string_literal: true
+
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
-authenticate :user, lambda { |u| u.admin? } do
-  mount Sidekiq::Web => '/sidekiq'
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
 
-  namespace :madmin do
-    resources :users
-    resources :announcements
-    resources :notifications
-    namespace :active_storage do
-      resources :blobs
+    namespace :madmin do
+      resources :users
+      resources :announcements
+      resources :notifications
+      namespace :active_storage do
+        resources :blobs
+      end
+      namespace :active_storage do
+        resources :attachments
+      end
+      resources :services
+      root to: 'dashboard#show'
     end
-    namespace :active_storage do
-      resources :attachments
-    end
-    resources :services
-    root to: "dashboard#show"
   end
-end
 
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
